21ae8deaea2791566fe3b79297c2f0447a2ef8c1
DripTrace <rpalm@driptrace.io>
2024-10-10 22:13:57 -0700

    (fix): replacing prod url

--- src/components/forms/settings/code-snippet.tsx
+++ src/components/forms/settings/code-snippet.tsx
@@ -27,16 +27,16 @@ const CodeSnippet = ({ id }: Props) => {
             border: none;
         }
     ')
-    iframe.src = "http://localhost:3000/chatbot"
+    iframe.src = "https://spread.vercel.app/chatbot"
     iframe.classList.add('chat-frame')
     document.body.appendChild(iframe)
     
     window.addEventListener("message", (e) => {
-        if(e.origin !== "http://localhost:3000") return null
+        if(e.origin !== "https://spread.vercel.app") return null
         let dimensions = JSON.parse(e.data)
         iframe.width = dimensions.width
         iframe.height = dimensions.height
-        iframe.contentWindow.postMessage("${id}", "http://localhost:3000/")
+        iframe.contentWindow.postMessage("${id}", "https://spread.vercel.app/")
     })
         `;
 
